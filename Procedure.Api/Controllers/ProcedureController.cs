7d16bb9693eed6ce8ae93c83aed5261b9cee05ce
Lucas Pacheco <lucaspsilveira@live.com>
2022-01-18 02:06:16 -0300

    Added procedure service

---
+++ Procedure.Api/Controllers/ProcedureController.cs
@@ -0,0 +1,34 @@
+using Hl7.Fhir.Rest;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Procedure.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProcedureController : ControllerBase
+    {
+        private readonly ILogger<ProcedureController> _logger;
+        private readonly FhirClient _client;
+
+        public ProcedureController(ILogger<ProcedureController> logger)
+        {
+            _logger = logger;
+            var settings = new FhirClientSettings
+            {
+                Timeout = 30,
+                PreferredFormat = ResourceFormat.Json,
+                VerifyFhirVersion = true,
+                PreferredReturn = Prefer.RespondAsync
+            };
+            _client = new FhirClient("http://hapi.fhir.org/baseR4", settings);
+        }
+
+        [HttpPost]
+        public async Task<ObjectResult> Post([FromBody] Hl7.Fhir.Model.Procedure procedure)
+        {
+            var response = await _client.CreateAsync(procedure);
+            return Ok(response);
+        }
+
+    }
+}
