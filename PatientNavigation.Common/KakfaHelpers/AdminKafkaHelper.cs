63ba059bc4ae9e7f511e421c38695fab72831eae
Lucas Pacheco <lucaspsilveira@live.com>
2022-01-19 00:12:40 -0300

    Added functionalities to publish event to kafka and create topics

---
+++ PatientNavigation.Common/KakfaHelpers/AdminKafkaHelper.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientNavigation.Common.KakfaHelpers
+{
+    public class AdminKafkaHelper
+    {
+        public static async Task CreateTopicAsync(string bootstrapServers, string topicName)
+        {
+            using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = bootstrapServers }).Build())
+            {
+                try
+                {
+                    await adminClient.CreateTopicsAsync(new TopicSpecification[] {
+                        new TopicSpecification { Name = topicName, ReplicationFactor = 1, NumPartitions = 1 } });
+                }
+                catch (CreateTopicsException e)
+                {
+                    Console.WriteLine($"An error occured creating topic {e.Results[0].Topic}: {e.Results[0].Error.Reason}");
+                }
+            }
+        }
+    }
+}
---
+++ PatientNavigation.Common/KakfaHelpers/EventsHelper.cs
@@ -0,0 +1,40 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientNavigation.Common.KakfaHelpers
+{
+    public class EventsHelper
+    {
+        private string _boostrapServers;
+        private readonly ClientConfig _clientConfig;
+        public EventsHelper(string boostrapServers)
+        {
+            _boostrapServers = boostrapServers;
+            _clientConfig = new()
+            {
+                BootstrapServers = _boostrapServers,
+                SecurityProtocol = SecurityProtocol.Plaintext
+            };
+        }
+
+        public async Task<DeliveryResult<Null, string>?> Produce(string topicName, string message)
+        {
+            using var producer = new ProducerBuilder<Null, string>(_clientConfig).Build();
+            return await producer.ProduceAsync(topicName, new Message<Null, string> { Value = message }).ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("An error has occured during publishing process. Details: " + task?.Exception?.Message ?? string.Empty);
+                    return task?.Result;
+                }
+                Console.WriteLine($"Wrote to offset: {task.Result.Offset}");
+                return task.Result;
+            });
+        }
+    }
+}
