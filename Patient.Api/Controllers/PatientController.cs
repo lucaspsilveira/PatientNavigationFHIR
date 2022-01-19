309543cca73ba204d88b117b4904f60c61b7b5d1
Lucas Pacheco <lucaspsilveira@live.com>
2022-01-19 00:13:10 -0300

    Added feature to publish message to kafka from patient api

--- Patient.Api/Controllers/PatientController.cs
+++ Patient.Api/Controllers/PatientController.cs
@@ -1,6 +1,8 @@
 using Hl7.Fhir.Rest;
 using Hl7.Fhir.Serialization;
 using Microsoft.AspNetCore.Mvc;
+using PatientNavigation.Common.KakfaHelpers;
+using System.Configuration;
 
 namespace Patient.Api.Controllers
 {
@@ -10,10 +12,18 @@ namespace Patient.Api.Controllers
     {
         private readonly ILogger<PatientController> _logger;
         private readonly FhirClient _client;
+        private readonly string _topicName;
+        private readonly EventsHelper _eventsHelper;
 
-        public PatientController(ILogger<PatientController> logger)
+        public PatientController(ILogger<PatientController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            
+            var boostrapServer = configuration.GetValue<string>("KafkaConfig:Servers");
+            _eventsHelper = new EventsHelper(boostrapServer);
+
+            _topicName = configuration.GetValue<string>("KafkaConfig:TopicName");
+
             var settings = new FhirClientSettings
             {
                 Timeout = 30,
@@ -28,8 +38,9 @@ namespace Patient.Api.Controllers
         [HttpPost]
         public async Task<ObjectResult> PostAsync([FromBody] Hl7.Fhir.Model.Patient patient)
         {
-            var result = await _client.CreateAsync(patient).ConfigureAwait(false);
-            return Ok(result.ToJson());
+            //var result = await _client.CreateAsync(patient).ConfigureAwait(false);
+            await _eventsHelper.Produce(_topicName, patient.ToJson());
+            return Ok("");
         }
     }
 }
